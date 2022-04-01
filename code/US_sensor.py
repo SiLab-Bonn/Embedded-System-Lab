1bf9c8d11c199685092ccd0b7b07dba9be880b90
hansk <krueger@physik.uni-bonn.de>
2022-04-01 10:57:07 +0200

    t

--- code/US_sensor.py
+++
@@ -1,7 +0,0 @@
-from gpiozero import DistanceSensor
-from time import sleep
-
-US = DistanceSensor(echo=5, trigger=4, max_distance=4, queue_len=1)
-while True:
-    print("Distance: %0.2f cm" %(US.distance * 100))
-    sleep(1)
