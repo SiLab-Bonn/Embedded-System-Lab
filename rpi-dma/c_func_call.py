5ff52a5c1c642220e0fa038fed272083d8311e26
HK <krueger@physik.uni-bonn.de>
2022-10-10 15:13:53 +0200

    test

--- rpi-dma/c_func_call.py
+++ rpi-dma/c_func_call.py
@@ -1,8 +1,10 @@
 import ctypes
 import time
+import matplotlib.pyplot as plt
 import RPi.GPIO as GPIO
 # GPIO.setmode(GPIO.BOARD) # pin header numbering
 GPIO.setmode(GPIO.BCM) # GPIO port numbering
+GPIO.setwarnings(False)
 
 ADC = ctypes.CDLL("./adc.so")
 
@@ -10,10 +12,10 @@ TRIGGER = 4
 GPIO.setup(TRIGGER, GPIO.OUT)
 GPIO.output(TRIGGER, GPIO.LOW)
 
-ADC.Hello.restype = ctypes.c_char_p
-print(ADC.Hello().decode())
+# ADC.Hello.restype = ctypes.c_char_p
+# print(ADC.Hello().decode())
 
-n_samples = 10
+n_samples = 1000
 time_base = 3 # 1 Msps
 wait_for_trigger = 0
 adc_data = (ctypes.c_uint16 * n_samples)()
@@ -24,9 +26,12 @@ GPIO.output(TRIGGER, GPIO.HIGH)
 ADC.take_data()
 GPIO.output(TRIGGER, GPIO.LOW)
 
-
 ADC.close_device()
 
-for i in range(n_samples):
-    print((adc_data[i]))
+# for i in range(n_samples):
+#     print((adc_data[i]))
 
+plt.ylabel('ADU')
+plt.xlabel('t[us]')
+plt.plot(adc_data)
+plt.show()
