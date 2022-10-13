46a63e2f564b31a577c87c617cd3a292e9f61646
Hans Krüger <krueger@physik.uni-bonn.de>
2022-10-13 20:53:54 +0200

    sar_adc pdf plot

--- rpi-dma/sar_adc.py
+++ rpi-dma/sar_adc.py
@@ -1,6 +1,8 @@
 import time
 import RPi.GPIO as GPIO
 import spidev as SPI
+import matplotlib.pyplot as plt
+import numpy as np
 
 GPIO.setmode(GPIO.BCM)
 
@@ -12,13 +14,16 @@ spi.max_speed_hz = 100000
 dac_resolution = 8 # resolution in bits
 
 SAMPLE = 4 # GPIO4
+GPIO.setwarnings(False)
 GPIO.setup(SAMPLE, GPIO.OUT)
 GPIO.output(SAMPLE, GPIO.LOW)
 
 COMP = 5  # GPIO5
 GPIO.setup(COMP, GPIO.IN)
 
-while True:
+adc_data = np.array([])
+
+for i in range(100000):
   # trigger sample switch
   GPIO.output(SAMPLE, GPIO.HIGH)
   time.sleep(0.0001)
@@ -29,7 +34,7 @@ while True:
 
   # succesive approximation loop from bit 7 (MSB) down to bit 0 (LSB)
   for dac_bit in reversed(range(dac_resolution)):
-    # add DAC bit value
+    # set next DAC bit value
     dac_value |= 1 << (dac_bit) 
     # write DAC value 
     spi.xfer([dac_value])
@@ -41,7 +46,13 @@ while True:
     #print(dac_bit, dac_value, result)
 
   spi.xfer([dac_value]) # write final DAC value with correct LSB
-  time.sleep(0.1)
+  adc_data = np.append(adc_data, [dac_value])
+  #time.sleep(0.1)
+
+#print(adc_data)
+
+plt.hist(adc_data, bins = range(0, 256, 1))
+plt.show()
 
 #print("dac_value", dac_value)
 
