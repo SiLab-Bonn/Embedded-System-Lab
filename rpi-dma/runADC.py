f626a707b6eabedff7229b6d9b6e8070d655af04
Hans Krüger <krueger@physik.uni-bonn.de>
2022-10-13 20:01:49 +0200

    home test

--- rpi-dma/runADC.py
+++ rpi-dma/runADC.py
@@ -15,8 +15,8 @@ GPIO.output(TRIGGER, GPIO.LOW)
 # ADC.Hello.restype = ctypes.c_char_p
 # print(ADC.Hello().decode())
 
-n_samples = 1000
-time_base = 3 # 1 Msps
+n_samples = 2000
+time_base = 1 # 1 Msps
 wait_for_trigger = 0
 adc_data = (ctypes.c_uint16 * n_samples)()
 
--- rpi-dma/sar_adc.py
+++ rpi-dma/sar_adc.py
@@ -7,7 +7,7 @@ GPIO.setmode(GPIO.BCM)
 spi = SPI.SpiDev()
 spi.open(0,0)  # (bus, device)
 spi.mode = 0
-spi.max_speed_hz = 1000000
+spi.max_speed_hz = 100000
 
 dac_resolution = 8 # resolution in bits
 
@@ -18,26 +18,32 @@ GPIO.output(SAMPLE, GPIO.LOW)
 COMP = 5  # GPIO5
 GPIO.setup(COMP, GPIO.IN)
 
-# trigger sample switch
-GPIO.output(SAMPLE, GPIO.HIGH)
-time.sleep(0.0001)
-GPIO.output(SAMPLE, GPIO.LOW)
+while True:
+  # trigger sample switch
+  GPIO.output(SAMPLE, GPIO.HIGH)
+  time.sleep(0.0001)
+  GPIO.output(SAMPLE, GPIO.LOW)
 
-# reset dac value
-dac_value = 0
+  # reset dac value
+  dac_value = 0
 
-# succesive approximation loop, MSB to LSB
-for dac_bit in reversed(range(dac_resolution)):
-  #set DAC value
-  dac_value |= 1 << (dac_bit) 
-  spi.xfer([dac_value])
+  # succesive approximation loop from bit 7 (MSB) down to bit 0 (LSB)
+  for dac_bit in reversed(range(dac_resolution)):
+    # add DAC bit value
+    dac_value |= 1 << (dac_bit) 
+    # write DAC value 
+    spi.xfer([dac_value])
 
-  # get result from comparator 
-  result = 147 >= dac_value #GPIO.input(COMP)
-  if not (result):
-    dac_value -= 1 << (dac_bit)
-  print(dac_bit, dac_value, result)
-print("dac_value", dac_value)
+    # get result from comparator 
+    result = GPIO.input(COMP)
+    if not (result): # input voltage is lower than DAC voltage
+      dac_value -= 1 << (dac_bit)  # substract DAC bit value
+    #print(dac_bit, dac_value, result)
+
+  spi.xfer([dac_value]) # write final DAC value with correct LSB
+  time.sleep(0.1)
+
+#print("dac_value", dac_value)
 
 
 
