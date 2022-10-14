a7cd800fbcd4650573959ee1182e6f36f101e0b2
HK <krueger@physik.uni-bonn.de>
2022-10-14 15:29:40 +0200

    void

--- rpi-dma/sar_adc.py
+++ rpi-dma/sar_adc.py
@@ -1,13 +1,13 @@
 import time
 import RPi.GPIO as GPIO
 import spidev as SPI
-# GPIO.setmode(GPIO.BOARD) # RPi.GPIO Layout verwenden (wie Pin-Nummern)
+
 GPIO.setmode(GPIO.BCM)
 
 spi = SPI.SpiDev()
 spi.open(0,0)  # (bus, device)
 spi.mode = 0
-spi.max_speed_hz = 10000000
+spi.max_speed_hz = 100000
 
 dac_resolution = 8 # resolution in bits
 
@@ -26,17 +26,20 @@ GPIO.output(SAMPLE, GPIO.LOW)
 # reset dac value
 dac_value = 0
 
-# succesive approximation loop, MSB to LSB
+# successive approximation loop, MSB to LSB
 for dac_bit in reversed(range(dac_resolution)):
   #set DAC value
   dac_value |= 1 << (dac_bit) 
   spi.xfer([dac_value])
 
   # get result from comparator 
-  result = 147 >= dac_value #GPIO.input(COMP)
+  result = GPIO.input(COMP)
   if not (result):
     dac_value -= 1 << (dac_bit)
   print(dac_bit, dac_value, result)
+spi.xfer([dac_value])
+    
+
 print("dac_value", dac_value)
 
 
