e6b5def303dc1d9e2de99f78b7feb54169ffaea5
Hans Krüger <krueger@physik.uni-bonn.de>
2022-10-20 20:49:36 +0200

    Removed __pycache_-

--- rpi-dma/fast_adc.py
+++ rpi-dma/fast_adc.py
@@ -1,8 +1,9 @@
 import ctypes
 import time
 import matplotlib.pyplot as plt
+import numpy as np
 import RPi.GPIO as GPIO
-# GPIO.setmode(GPIO.BOARD) # pin header numbering
+
 GPIO.setmode(GPIO.BCM) # GPIO port numbering
 GPIO.setwarnings(False)
 
@@ -15,23 +16,61 @@ GPIO.output(TRIGGER, GPIO.LOW)
 # ADC.Hello.restype = ctypes.c_char_p
 # print(ADC.Hello().decode())
 
-n_samples = 2000
-time_base = 1 # 1 Msps
+n_samples = 20000
+time_base = 3 # 1 Msps
 wait_for_trigger = 0
 adc_data = (ctypes.c_uint16 * n_samples)()
+adc_data_array = np.array([])
 
 ADC.init_device(adc_data, n_samples, time_base, wait_for_trigger)
 
-GPIO.output(TRIGGER, GPIO.HIGH)
-ADC.take_data()
-GPIO.output(TRIGGER, GPIO.LOW)
-
-ADC.close_device()
-
-# for i in range(n_samples):
-#     print((adc_data[i]))
+for i in range(100):
+  #GPIO.output(TRIGGER, GPIO.HIGH)
+  ADC.take_data()
+  #GPIO.output(TRIGGER, GPIO.LOW)
+  adc_data_array = np.append(adc_data_array, adc_data)
 
 plt.ylabel('ADU')
 plt.xlabel('t[us]')
 plt.plot(adc_data)
+#plt.show()
+# ADC.close_device()
+
+# ADC.init_device(adc_data, 1000, time_base, wait_for_trigger)
+
+# GPIO.output(TRIGGER, GPIO.HIGH)
+# ADC.take_data()
+# GPIO.output(TRIGGER, GPIO.LOW)
+
+# set limits (cut the over- and underflow bins)
+lower_bound =  100
+upper_bound = 3300
+
+# count histogram
+adc_hist, bin_edges = np.histogram(adc_data_array, bins=upper_bound-lower_bound, range=(lower_bound,upper_bound-1))
+
+# average bin height 
+adc_hist_avg = np.average(adc_hist)
+
+# differential non-linearity
+adc_dnl = (adc_hist-adc_hist_avg)/adc_hist_avg
+
+# integral non-linearity
+adc_inl = np.cumsum(adc_dnl)
+
+# prepare plots
+figure, plot = plt.subplots(3, 1)
+plot[0].stairs(adc_hist, bin_edges, fill=True)
+plot[0].set_xticks(range(0, 4100, 512))
+plot[0].set_ylabel("Counts")
+plot[1].stairs(adc_dnl, bin_edges)
+plot[1].set_ylim(-2, 2)
+plot[1].set_xticks(range(0, 4100, 512))
+plot[1].set_ylabel("DNL")
+plot[2].stairs(adc_inl, bin_edges)
+plot[2].set_ylim(-20, 20)
+plot[2].set_xticks(range(0, 4100, 512))
+plot[2].set_ylabel("INL")
 plt.show()
+
+ADC.close_device()
--- rpi-dma/tdr.py
+++ rpi-dma/tdr.py
@@ -1,54 +1,96 @@
 import time
 import RPi.GPIO as GPIO
 import spidev as SPI
-# GPIO.setmode(GPIO.BOARD) # RPi.GPIO Layout verwenden (wie Pin-Nummern)
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 spi = SPI.SpiDev()
 spi.open(0,0)  # (bus, device)
 spi.mode = 0
-spi.max_speed_hz = 10000000
+spi.max_speed_hz = 5000000
 
-dac_resolution = 12 # resolution in bits
-delay_size = 1 # numer of delay steps
-
-spi_array = bytearray(6)
-dac_value   = 0
-delay_value = 0
-spi_array = [dac_value >> 8, dac_value & 0xff, delay_value >> 8, delay_value & 0xff]
- 
-
+COMP = 5
+GPIO.setup(COMP, GPIO.IN)
 TRIGGER = 4
 GPIO.setup(TRIGGER, GPIO.OUT)
 GPIO.output(TRIGGER, GPIO.LOW)
 
-COMP = 5
-GPIO.setup(COMP, GPIO.IN)
+def reverse_bits(num, bits):
+    result = 0
+    for i in range(bits):
+        result = (result << 1) + (num & 1)
+        num >>= 1
+    return result
 
-for t in range(delay_size):
-  #set delay value
-  delay_value = t
-  spi_array = [dac_value >> 8, dac_value & 0xff, delay_value >> 8, delay_value & 0xff]
-  spi.xfer(spi_array)
-  #reset dac output 
-  dac_value = 0
+def update_spi_regs(threshold, pulse_delay, sample_delay):
+  # MCP4811 DAC samples first 16 bits after CS falling edge (MSB first)
+  # SY89297 delay line samples last 20 bits before CS rising edge (LSB first)
+  # Both device connect in parallel to the MOSI line (not daisy chained!)
+  spi_data = ((threshold | dac_cmd) << 24) + \
+             ((0x3ff & reverse_bits(pulse_delay,  10)) << 10) + \
+             ((0x3ff & reverse_bits(sample_delay, 10)) << 0)
+  #print(bin(spi_data))
+  spi.xfer(bytearray(spi_data.to_bytes(5, byteorder='big')))
 
-  for dac_bit in reversed(range(dac_resolution)):
+dac_resolution = 12 # resolution in bits
+delay_size = 1 # number of delay steps
+
+dac_cmd      = 0x3000 # DAC enable, gain = 1: VDAC = [0..2047]mV
+threshold    = 2048
+pulse_delay  =  0
+sample_delay =    0
+max_delay    =  1023  
+max_threshold = 4095
+delay_unit   =    5
+
+# update_spi_regs(3000, pulse_delay, sample_delay)
+# GPIO.output(TRIGGER, GPIO.HIGH)
+# GPIO.output(TRIGGER, GPIO.LOW)
+
+amplitude_data = np.array([])
+time_steps = np.array([])
+
+for sample_delay in tqdm(range(max_delay)):
+  time_steps = np.append(time_steps, [sample_delay * delay_unit])
+  threshold = 2048 # start with mid-level DAC output for SAR ADC conversion
+
+  for dac_bit in reversed(range(dac_resolution)): # SAR conversion from MSB to LSB
     #set DAC value
-    dac_value |= 1 << (dac_bit)
-    spi_array = [dac_value >> 8, dac_value & 0xff, delay_value >> 8, delay_value & 0xff]
-    spi.xfer(spi_array)
+    threshold |= 1 << (dac_bit)
+    # update comparator threshold
+    update_spi_regs(threshold, pulse_delay, sample_delay)
 
     # trigger pulse step
     GPIO.output(TRIGGER, GPIO.HIGH)
+    time.sleep(0.00001)
+    # sample comparator result
+    result = GPIO.input(COMP)
+    # reset pulse output
     GPIO.output(TRIGGER, GPIO.LOW)
 
-    result = 253 >= dac_value #GPIO.input(COMP)
-    print(dac_bit, dac_value, result)
-    if not result:
-      dac_value -= 1 << (dac_bit)
-print("dac_value", dac_value)
+    if result: # set next DAC bit
+      threshold -= 1 << (dac_bit)
+
+  amplitude_data = np.append(amplitude_data, [max_threshold - threshold])
+
+# plot the waveform data
+fig, waveform = plt.subplots(2,1)
+waveform[0].plot(time_steps, amplitude_data)
+waveform[0].set_xlabel("time [ps]")
+waveform[0].set_ylabel("voltage [#DAC]")
+waveform[0].set_ylim(2500, 4500)
+waveform[1].plot(time_steps, amplitude_data)
+waveform[1].set_xlabel("time [ps]")
+waveform[1].set_ylabel("voltage [#DAC]")
+waveform[1].set_ylim(3500, 4000)
+
+plt.show()
 
 
+spi.close()
 
 
