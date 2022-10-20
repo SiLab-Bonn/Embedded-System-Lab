f7f761c542dc86502bc6be5a66e4bb83e8c8d978
HK <krueger@physik.uni-bonn.de>
2022-10-20 12:09:06 +0200

    first pulse sampler response

--- rpi-dma/tdr.py
+++ rpi-dma/tdr.py
@@ -4,10 +4,10 @@ import spidev as SPI
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-# spi = SPI.SpiDev()
-# spi.open(0,0)  # (bus, device)
-# spi.mode = 0
-# spi.max_speed_hz = 100000
+spi = SPI.SpiDev()
+spi.open(0,0)  # (bus, device)
+spi.mode = 0
+spi.max_speed_hz = 1000000
 
 COMP = 5
 GPIO.setup(COMP, GPIO.IN)
@@ -15,67 +15,56 @@ TRIGGER = 4
 GPIO.setup(TRIGGER, GPIO.OUT)
 GPIO.output(TRIGGER, GPIO.LOW)
 
-SCK = 11
-GPIO.setup(SCK, GPIO.OUT)
-GPIO.output(SCK, GPIO.LOW)
-SDO = 10
-GPIO.setup(SDO, GPIO.OUT)
-GPIO.output(SDO, GPIO.LOW)
-CS0_B = 8
-GPIO.setup(CS0_B, GPIO.OUT)
-GPIO.output(CS0_B, GPIO.HIGH)
+def reverse_bits(num, bits):
+    result = 0
+    for i in range(bits):
+        result = (result << 1) + (num & 1)
+        num >>= 1
+    return result
 
-def spixfer(data, num_bits):
-  GPIO.output(CS0_B, GPIO.LOW)
-  for i in reversed(range(num_bits)):
-    GPIO.output(SDO, 0x01 & (data >> i))
-
-    GPIO.output(SCK, GPIO.HIGH)
-
-    GPIO.output(SCK, GPIO.LOW)
-    
-  GPIO.output(CS0_B, GPIO.HIGH)
-
+def update_spi_regs(threshold, pulse_delay, sample_delay):
+  # MCP4811 DAC samples first 16 bits after CS falling edge (MSB first)
+  # SY89297 delay line samples last 20 bits before CS rising edge (LSB first)
+  # Both device connect in parallel to the MOSI line (not daisy chained!)
+  spi_data = ((threshold | dac_cmd) << 24) + \
+             ((0x3ff & reverse_bits(pulse_delay,  10)) << 10) + \
+             ((0x3ff & reverse_bits(sample_delay, 10)) << 0)
+  #print(bin(spi_data))
+  spi.xfer(bytearray(spi_data.to_bytes(5, byteorder='big')))
 
 dac_resolution = 12 # resolution in bits
 delay_size = 1 # number of delay steps
 
-spi_array = bytearray(5)  # holds 2 x 10 bit delay + 16 bit DAC command/data
-dac_cmd     = 0x1000 # DAC enable, gain = 1: VDAC = [0..2024]mV
-dac_value   = 0x0080
-delayA_value = 0x200 #0x07f
-delayB_value = 0x200
-spi_data = ((dac_value | dac_cmd) << 20) + ((0x3ff & delayA_value) << 10) + (0x3ff & delayB_value)
-spi_array = spi_data.to_bytes(5, byteorder='big')
-#print(bin(spi_data))
+dac_cmd      = 0x3000 # DAC enable, gain = 1: VDAC = [0..2047]mV
+threshold    = 2048
+pulse_delay  = 100 #0x07f
+sample_delay = 1000
 
-spixfer(spi_data, 36)
-#spi.xfer(spi_array)
+# update_spi_regs(3000, pulse_delay, sample_delay)
+# GPIO.output(TRIGGER, GPIO.HIGH)
+# GPIO.output(TRIGGER, GPIO.LOW)
 
-# for t in range(delay_size):
-#   #set delay value
-#   delay_value = t
-#   spi_array = [(dac_value >> 8) | dac_cmd, dac_value & 0xff, delayB_value >> 8, delayB_value & 0xff, delayA_value >> 8, delayA_value & 0xff]
-#   spi.xfer(spi_array)
-#   #reset dac output 
-#   dac_value = 0
+for t in range(1000):
+  #set delay value
+  pulse_delay = t
+  threshold = 2048 # start with mid-level DAC output for SAR ADC conversion
 
-#   for dac_bit in reversed(range(dac_resolution)):
-#     #set DAC value
-#     dac_value |= 1 << (dac_bit)
-#     spi_array = [(dac_value >> 8) | dac_cmd, dac_value & 0xff, delayB_value >> 8, delayB_value & 0xff, delayA_value >> 8, delayA_value & 0xff]
-#     spi.xfer(spi_array)
+  for dac_bit in reversed(range(dac_resolution)):
+    #set DAC value
+    threshold |= 1 << (dac_bit)
+    update_spi_regs(threshold, pulse_delay, sample_delay)
 
-#     # trigger pulse step
-#     GPIO.output(TRIGGER, GPIO.HIGH)
-#     GPIO.output(TRIGGER, GPIO.LOW)
+    # trigger pulse step
+    GPIO.output(TRIGGER, GPIO.HIGH)
+    GPIO.output(TRIGGER, GPIO.LOW)
+    GPIO.output(TRIGGER, GPIO.HIGH)
+    
+    result = GPIO.input(COMP)
+    #print(dac_bit, threshold, result)
+    if result:
+      threshold -= 1 << (dac_bit)
+  print("input level", threshold)
 
-#     result = 253 >= dac_value #GPIO.input(COMP)
-#     print(dac_bit, dac_value, result)
-#     if not result:
-#       dac_value -= 1 << (dac_bit)
-# print("dac_value", dac_value)
-
-
+spi.close()
 
 
