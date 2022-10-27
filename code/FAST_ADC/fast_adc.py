f5999bbd385ea7af3ad1174e68e12104c0ed0b36
HK <krueger@physik.uni-bonn.de>
2022-10-27 18:23:06 +0200

    fast adc improvement

--- code/FAST_ADC/fast_adc.py
+++ code/FAST_ADC/fast_adc.py
@@ -8,7 +8,16 @@ from tqdm import tqdm
 GPIO.setmode(GPIO.BCM) # GPIO port numbering
 GPIO.setwarnings(False)
 
-ADC = ctypes.CDLL("../lib/fast_adc.so")
+ADC = ctypes.CDLL("../lib/fast_adc.so")  # access to c-library for fast ADC access with DMA support
+ADC.get_time_base.restype = ctypes.c_float # set return type for time base getter
+
+time_dict={
+  "200k" : 1,
+  "500k" : 2,
+  "1M"   : 3,
+  "2M"   : 4,
+  "5M"   : 5
+}
 
 TRIGGER = 4
 GPIO.setup(TRIGGER, GPIO.OUT)
@@ -17,21 +26,22 @@ GPIO.output(TRIGGER, GPIO.LOW)
 # ADC.Hello.restype = ctypes.c_char_p
 # print(ADC.Hello().decode())
 
-n_samples = 1
-time_base = 5 # 1 Msps
-wait_for_trigger = 0
+n_samples = 10000 # number of samples 
+#time_base = 1 # [1, 2, 3, 4, 5] = [200k, 500k 1M, 2M, 5M] samples per second
+wait_for_trigger = 0 # 0: take data immediately, 1: wait for line 24 (DREQ) to go high
 adc_data = (ctypes.c_uint16 * n_samples)()
-adc_data_array = np.array([])
 
-ADC.init_device(adc_data, n_samples, time_base, wait_for_trigger)
+ADC.init_device(adc_data, n_samples, 4, wait_for_trigger)
+ADC.take_data()
 
-for i in tqdm(range(100000)):
-  ADC.take_data()
-  adc_data_array = np.append(adc_data_array, adc_data)
-  
+time_base = ADC.get_time_base()
+time_data = np.arange(0, n_samples * time_base, time_base)
+adc_data  = np.array(adc_data)
+
+# plot ADC waveform data 
 plt.ylabel('ADU')
 plt.xlabel('t[us]')
-plt.plot(adc_data)
+plt.plot(time_data, adc_data)
 plt.show()
 
 ADC.close_device()
