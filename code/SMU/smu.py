30fbc80841c26641cfc69c0b2cf04203348d863f
Hans Krüger <krueger@physik.uni-bonn.de>
2022-12-02 15:38:55 +0100

    MOSFET IV curve

--- code/SMU/smu.py
+++ code/SMU/smu.py
@@ -20,19 +20,50 @@ def SetVoltage(channel, volt):
   #print(i2c_data)
   DAC.write(i2c_data)
 
-def GetCurrent():
-  i2c_data = ADC.read(4)
-  curr_ch0 = 0x0fff & int.from_bytes(i2c_data[0:2],"big")
-  curr_ch1 = 0x0fff & int.from_bytes(i2c_data[2:4],"big")
-  #print(curr_ch0, curr_ch1)  
-  return (curr_ch0, curr_ch1)
+def GetCurrent(channel, average=False):
+  if average:
+    setup_data = bytes([0x21 | (channel << 1)])
+  else:
+    setup_data = bytes([0x61 | (channel << 1)])
+  ADC.write(setup_data)
 
-for voltage in np.arange(0, 4.1, 0.1):
-  SetVoltage(0, voltage)
-  current0, current1 = GetCurrent()
-  print(voltage, current0)
+  if average:
+    i2c_data = ADC.read(16)
+    current = 0
+    for i in range(8):
+      current += 0x0fff & int.from_bytes(i2c_data[(2*i):(2*i+2)],"big")
+    current = current >> 3
+  else:
+    i2c_data = ADC.read(2)
+    current = 0x0fff & int.from_bytes(i2c_data,"big")
 
+  return current
 
+voltage_data = np.arange(0, 4.1, 0.05)
+current0_data = np.array([])
+current1_data = np.array([])
+
+SetVoltage(1, 1)  # drain
+
+for voltage in voltage_data:
+  SetVoltage(0, voltage) # gate
+  current0 = GetCurrent(0, average=True) 
+  current0_data = np.append(current0_data, current0)  
+ 
+SetVoltage(1, 1.1)  # drain
+
+for voltage in voltage_data:
+  SetVoltage(0, voltage) # gate
+  current1 = GetCurrent(0, average=True)
+  current1_data = np.append(current1_data, current1)
+
+fig, ax = plt.subplots()
+ax.plot(voltage_data, current0_data)
+ax.plot(voltage_data, current1_data)
+
+ax.set(xlabel='Voltage (V)', ylabel='Current (uA)', title='I-V Curve')
+ax.grid()
+plt.show()
 
 DAC.close() # close device
 ADC.close()
