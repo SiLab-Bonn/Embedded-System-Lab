1318817a3adf95df1ad690b9a27ec20b7e36c308
Hans Krüger <krueger@physik.uni-bonn.de>
2023-04-18 12:03:32 +0200

    added DAC DN/INL

--- code/SAR_ADC/sar_adc_solution.py
+++ code/SAR_ADC/sar_adc_solution.py
@@ -30,8 +30,6 @@ num_samples = 1000 # number of ADC samples for INL/DNL analysis
 
 resolution = dac_resolution  # default resolution 
 
-adc_samples = np.zeros(num_samples) # create array to store the ADC data
-
 def write_dac(value):
   spi.xfer([value])  # write DAC register 
 
@@ -44,7 +42,7 @@ def sar_loop(resolution = dac_resolution):
     dac_register |= 1 << (dac_bit)             # set next DAC register bit 
     write_dac(dac_register)                    # update DAC register
     dac_register_array.append(dac_register >> res_offest)    # store current dac register value for later plotting
-    time.sleep(0.0001)                         # wait for comparator to settle
+    time.sleep(0.001)                         # wait for comparator to settle
     comp_out = GPIO.input(COMP)                # read comparator output
     if (not comp_out):                         # subtract current DAC bit if DAC voltage is higher than input voltage
       dac_register -= 1 << (dac_bit)           
@@ -58,7 +56,9 @@ def read_adc(resolution = dac_resolution):
   GPIO.output(SAMPLE, GPIO.LOW)  # open sample switch (hold mode)
   return sar_loop(resolution)
 
-def nl_analysis(resolution = dac_resolution):
+def adc_analysis(resolution = dac_resolution):
+  adc_samples = np.zeros(num_samples) # create array to store the ADC data
+
   # loop for number of samples
   for i in tqdm(range(num_samples)):
     adc_value, __ = read_adc(resolution)
@@ -103,10 +103,44 @@ def nl_analysis(resolution = dac_resolution):
   plot[2].set_ylabel("INL")
   plt.show()
 
+def dac_analysis(dac_binary_voltages_list):
+  # plot dac transfer function from mesaured output voltages
+  dac_bits  = len(dac_binary_voltages_list)  # number of bits
+  dac_steps = 1 << dac_bits                  # number of DAC codes
+  x = [i for i in (range(dac_steps))]        # dac code array
+  dac_voltage_list = []                      # prepare output voltage array
+
+  for i in range(dac_steps):                 # calculate DAC output voltages from binray weights
+    dac_voltage = 0
+    for bit in range(dac_bits):
+      dac_voltage = dac_voltage + (0x01 & (i >> bit)) * dac_binary_voltages_list[bit]
+    dac_voltage_list.append(dac_voltage)
+
+  dac_diff = np.diff(dac_voltage_list)       # difference between voltage steps
+  dac_diff_avg = np.average(dac_diff)        # average difference
+  dac_dnl = (dac_diff - dac_diff_avg)/dac_diff_avg  # calculate DNL
+  dac_inl = np.cumsum(dac_dnl)               # calcultae INL
+
+  #plt.ion()
+  fig, plot = plt.subplots(3,1)
+  plot[0].set(ylabel='DAC output voltage', title='DAC Analysis')
+  plot[0].grid()
+  plot[0].step(x, dac_voltage_list, where='mid')
+  plot[0].set_xticks(range(0, dac_steps+1,  dac_steps>>3))  
+  plot[1].set(ylabel='DNL')
+  plot[1].grid()
+  plot[1].step(x[1:], dac_dnl, where='mid')  
+  plot[1].set_xticks(range(0, dac_steps+1,  dac_steps>>3))  
+  plot[2].set(xlabel='DAC code', ylabel='INL')
+  plot[2].grid()
+  plot[2].step(x[1:], dac_inl, where='mid')  
+  plot[2].set_xticks(range(0, dac_steps+1,  dac_steps>>3))  
+  plt.show()
 
 
 while 1:
-  print("Select:\n[1] Write DAC\n[2] Read ADC\n[3] DAC loop\n[4] Read ADC and plot\n[5] Plot INL / DNL\n[6] Set resolution (default = 8)\n[q] Exit")
+  print("Select:\n[1] Write DAC\n[2] Read ADC\n[3] DAC loop\n[4] Read ADC and plot\n\
+[5] Analyze ADC INL / DNL\n[6] Set resolution (default = 8)\n[7] Analyze DAC INL / DNL\n[q] Exit")
   x = input()
 
   if (x == '1'):
@@ -153,16 +187,22 @@ while 1:
 
   if (x == '5'):
     while 1:
-      y = input("Press enter to run INL/DNL analysis ('q' to exit): ")
+      y = input("Enter sample number to run INL/DNL analysis ('q' to exit): ")
       if (y == 'q'):
         break    
-      nl_analysis(resolution)
+      num_samples = int(y)
+      adc_analysis(resolution)
 
   if (x == '6'):
       y = int(input("Set ADC/DAC resolution (1-8)"))
       if y in range(1,8):
         resolution = y
         print("Resolution set to %", y)
+  
+  if (x == '7'):
+      dac_voltage_list = [float(item) for item in input("Enter measured DAC output voltages (LSB to MSB):").split()]
+      print(dac_voltage_list)
+      dac_analysis(dac_voltage_list)
 
   if (x =='q'):  
     break
