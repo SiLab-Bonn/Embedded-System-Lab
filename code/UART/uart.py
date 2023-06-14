1e07e6c4f61b7b7c2c2825d175d8783339b0f27c
HK <krueger@physik.uni-bonn.de>
2023-06-14 10:44:00 +0200

    added UART loopback code

--- code/UART/uart.py
+++ code/UART/uart.py
@@ -2,18 +2,18 @@ import serial
 import time
 
 ser = serial.Serial('/dev/ttyS0', 115200)
-ser.open()
+#ser.open()
 if ser.is_open == False:
-     print("Could not open serial port.")
-     exit()
+  print("Could not open serial port.")
+  exit()
 
 ser.reset_input_buffer()
-        
+    
 while True:    
-  if ser.in_waiting > 1:
-    print(ser.readall())
-
-except KeyboardInterrupt:   # Ctrl+C
-
-if ser != None:
-  ser.close()
+  if (ser.inWaiting() > 0):
+      data_str = ser.read(ser.inWaiting()).decode('ascii') 
+      print("Received:", data_str)   
+  
+  key = input("Transmit: ").encode()
+  ser.write(key)
+  ser.flush()
