d1051a9ebbe8b33013d74b0b50fa345fc9e0f3ec
Hans Krüger <krueger@physik.uni-bonn.de>
2023-07-25 17:22:28 +0200

    s-curve

--- code/AFE/afe.py
+++ code/AFE/afe.py
@@ -21,12 +21,12 @@ GPIO.setup(INJECT, GPIO.OUT)
 GPIO.output(INJECT, GPIO.LOW)
 
 def update_spi_regs(threshold, injected_signal, time_constant):
-  # MCP4822/11 DAC samples first 16 bits after CS falling edge (MSB first)
-  # SN74HCS594 shift register samples last 8 bits before CS rising edge (MSB first)
+  # MCP4822/11 DAC samples *first* 16 bits after CS falling edge (MSB first)
+  # CPLD SPI shift register *samples* last 8 bits before CS rising edge (MSB first)
   # Both device connect in parallel to the MOSI line (not daisy chained!)
 
-  dac_cmd_a      = 0x1000 # channel A, DAC enable, gain = 2: VDAC = [0..4095]mV
-  dac_cmd_b      = 0x9000 # channel B, DAC enable, gain = 2: VDAC = [0..4095]mV
+  dac_cmd_a      = 0x3000 # channel A, DAC enable, gain = 1: VDAC = [0..2047]mV, 0.5 mV LSB
+  dac_cmd_b      = 0xb000 # channel B, DAC enable, gain = 1: VDAC = [0..2047]mV, 0.5 mV LSB
 
   #set DAC channel A (threshold voltage)
   spi_data = ((((0xfff & threshold) | dac_cmd_a) << 8) + \
@@ -40,15 +40,36 @@ def update_spi_regs(threshold, injected_signal, time_constant):
   #print(bin(spi_data)[2:].zfill(24))
   spi.xfer(bytearray(spi_data.to_bytes(3, byteorder='big')))
 
-update_spi_regs(600, 10, 6)
+charge    = 60
+threshold = 2800
 
-while True:
-  for charge in range(100):
-    update_spi_regs(1600, charge + 150, 3)
-    for i in range (10): 
-      GPIO.output(INJECT, GPIO.HIGH)
-      time.sleep(0.001)
-      GPIO.output(INJECT, GPIO.LOW)
-      time.sleep(0.001)
+GPIO.output(INJECT, GPIO.LOW)
+
+hit_data = []
+threshold_data = []
+
+for threshold in tqdm(reversed(range(2900, 3200, 2))):
+  threshold_data.append(threshold)
+#for charge in range(40, 80, 1):
+  update_spi_regs(threshold, charge, 4)
+  hit_count = 0
+  for i in range (100): 
+    GPIO.output(INJECT, GPIO.HIGH)
+    time.sleep(0.0005) 
+    if (GPIO.input(COMP)):
+      hit_count = hit_count + 1
+    GPIO.output(INJECT, GPIO.LOW)
+    time.sleep(0.0005)
+  hit_data.append(hit_count)
+
+fig, ax = plt.subplots()
+ax.plot(threshold_data, hit_data)
+
+ax.set(xlabel='threshold (DAC)', ylabel='hit count', title='S-Curve')
+ax.grid()
+
+#fig.savefig("test.png")
+plt.show()
 
 spi.close()
+GPIO.cleanup()
