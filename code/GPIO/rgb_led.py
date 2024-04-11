318589bdd939bfce0e9cd0a979b9e3669962efb6
Hans Krüger <krueger@physik.uni-bonn.de>
2024-04-11 16:51:18 +0200

    .

--- code/GPIO/rgb_led.py
+++ code/GPIO/rgb_led.py
@@ -2,7 +2,7 @@ import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM) 
 
-LED_R = 5
+LED_R = 25
 LED_G = 26
 LED_B = 27
 BTN   = 4
@@ -17,23 +17,24 @@ GPIO.output(LED_G, GPIO.LOW)
 GPIO.output(LED_B, GPIO.LOW)
 
 def wait_for_btn_press_and_release():
-    while (GPIO.input(BTN) == True):   # pressed
-        pass
-    while (GPIO.input(BTN) == False):  # not pressed
-        pass
+  while (GPIO.input(BTN) == True):   # pressed
+    pass
+  while (GPIO.input(BTN) == False):  # not pressed
+    pass
+  time.sleep(0.1)
 
 try:
-    while 1:
-        GPIO.output(LED_R, GPIO.HIGH)
-        wait_for_btn_press_and_release()
-        GPIO.output(LED_R, GPIO.LOW)
-        GPIO.output(LED_G, GPIO.HIGH)
-        wait_for_btn_press_and_release()
-        GPIO.output(LED_G, GPIO.LOW)
-        GPIO.output(LED_B, GPIO.HIGH)
-        wait_for_btn_press_and_release()
-        GPIO.output(LED_B, GPIO.LOW)
+  while 1:
+    GPIO.output(LED_R, GPIO.HIGH)
+    wait_for_btn_press_and_release()
+    GPIO.output(LED_R, GPIO.LOW)
+    GPIO.output(LED_G, GPIO.HIGH)
+    wait_for_btn_press_and_release()
+    GPIO.output(LED_G, GPIO.LOW)
+    GPIO.output(LED_B, GPIO.HIGH)
+    wait_for_btn_press_and_release()
+    GPIO.output(LED_B, GPIO.LOW)
 except KeyboardInterrupt:
-    print ("bye")
-    GPIO.cleanup()
-    pass
+  print ("bye")
+  GPIO.cleanup()
+  pass
