6fde19afaf66864d553801445dfcf59668cb3813
Hans Krüger <krueger@physik.uni-bonn.de>
2024-04-12 15:31:49 +0200

    adjusted AF mode for cam

--- code/picamera_test.py
+++ code/picamera_test.py
@@ -1,11 +1,14 @@
 from picamera2 import Picamera2
-import libcamera
+from libcamera import controls, Transform
+import time
 
 cam = Picamera2()
 config = cam.create_preview_configuration(main={"size": (1600, 1200)})
-config["transform"] = libcamera.Transform(hflip=1, vflip=1)
+config["transform"] = Transform(hflip=1, vflip=1)
 cam.configure(config)
-cam.set_controls({"AfMode": libcamera.controls.AfModeEnum.Continuous})
+cam.set_controls({"AfMode": controls.AfModeEnum.Auto, "AfTrigger": controls.AfTriggerEnum.Start})
+#cam.set_controls({"AfMode": libcamera.controls.AfModeEnum.Manual})
 cam.start(show_preview=True)
+time.sleep(1)
 input()
 cam.stop()
