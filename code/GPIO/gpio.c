3c4b4b2e2f62f8459a0bece06b8440cb45926307
Hans Krüger <krueger@physik.uni-bonn.de>
2024-04-16 19:32:28 +0200

    .

--- code/GPIO/gpio.c
+++ code/GPIO/gpio.c
@@ -39,6 +39,7 @@ uint32_t *gplev0;
 int setup_gpio_regs()
 {
   uint32_t  gpio_phys_addr;  // GPIO register CPU bus address 
+
   int file_descriptor;  // handle for memory mapping
 
   // calculate the physical address from the bus address
@@ -152,52 +153,52 @@ void set_gpio_out(int pin, int level)
 int main()
 {
 // example using direct register access  
-  uint32_t register_value;
-  uint32_t gpio_phys_addr;   // GPIO register CPU bus address 
-  int      file_descriptor;  // handle for memory mapping
+//   uint32_t register_value;
+//   uint32_t gpio_phys_addr;   // GPIO register CPU bus address 
+//   int      file_descriptor;  // handle for memory mapping
 
-  // calculate the physical address from the bus address
-  gpio_phys_addr = GPIO_BASE - BUS_REG_BASE + PHYS_REG_BASE;
+//   // calculate the physical address from the bus address
+//   gpio_phys_addr = GPIO_BASE - BUS_REG_BASE + PHYS_REG_BASE;
 
-  // get a handle to the physical memory space
-  file_descriptor = open("/dev/mem", O_RDWR|O_SYNC|O_CLOEXEC);  // requires root permissions ("sudo ...")
+//   // get a handle to the physical memory space
+//   file_descriptor = open("/dev/mem", O_RDWR|O_SYNC|O_CLOEXEC);  // requires root permissions ("sudo ...")
 
-  // allocate virtual memory and map the physical address to it
-  gpio_virt_addr_ptr = mmap(0, 0x1000, PROT_WRITE|PROT_READ, MAP_SHARED, file_descriptor, gpio_phys_addr);
-  close(file_descriptor);
+//   // allocate virtual memory and map the physical address to it
+//   gpio_virt_addr_ptr = mmap(0, 0x1000, PROT_WRITE|PROT_READ, MAP_SHARED, file_descriptor, gpio_phys_addr);
+//   close(file_descriptor);
 
-  // define variables to access the specific registers
-  gpfsel0 = (uint32_t*)((void *)gpio_virt_addr_ptr + GPIO_FSEL0);
-  gpfsel1 = (uint32_t*)((void *)gpio_virt_addr_ptr + GPIO_FSEL1);
-  gpfsel2 = (uint32_t*)((void *)gpio_virt_addr_ptr + GPIO_FSEL2);
-  gpset0  = (uint32_t*)((void *)gpio_virt_addr_ptr + GPIO_SET0);
-  gpclr0  = (uint32_t*)((void *)gpio_virt_addr_ptr + GPIO_CLR0);
-  gplev0  = (uint32_t*)((void *)gpio_virt_addr_ptr + GPIO_LEV0);
+//   // define variables to access the specific registers
+//   gpfsel0 = (uint32_t*)((void *)gpio_virt_addr_ptr + GPIO_FSEL0);
+//   gpfsel1 = (uint32_t*)((void *)gpio_virt_addr_ptr + GPIO_FSEL1);
+//   gpfsel2 = (uint32_t*)((void *)gpio_virt_addr_ptr + GPIO_FSEL2);
+//   gpset0  = (uint32_t*)((void *)gpio_virt_addr_ptr + GPIO_SET0);
+//   gpclr0  = (uint32_t*)((void *)gpio_virt_addr_ptr + GPIO_CLR0);
+//   gplev0  = (uint32_t*)((void *)gpio_virt_addr_ptr + GPIO_LEV0);
 
-  register_value  =  *gpfsel2;   // read the current GPIFSEL2 register setting 
-  //printf("GPFSEL2: 0x%08x\n", register_value);
-  register_value &= ~(0x7 << (7*3)); // clear the GPIO27 mode bits
-  register_value |=  (0x1 << (7*3)); // set the GPIO27 mode bits to mode 1 (output)
-  *gpfsel2 = register_value;     // set the new GPIFSEL2 register setting
-  //printf("GPFSEL2: 0x%08x\n", *gpfsel2);
+//   register_value  =  *gpfsel2;   // read the current GPIFSEL2 register setting 
+//   //printf("GPFSEL2: 0x%08x\n", register_value);
+//   register_value &= ~(0x7 << (7*3)); // clear the GPIO27 mode bits
+//   register_value |=  (0x1 << (7*3)); // set the GPIO27 mode bits to mode 1 (output)
+//   *gpfsel2 = register_value;     // set the new GPIFSEL2 register setting
+//   //printf("GPFSEL2: 0x%08x\n", *gpfsel2);
 
-  *gpset0 = 1 << 27;  // set output to '1'
+//   *gpset0 = 1 << 27;  // set output to '1'
+//   sleep(1);
+//   *gpclr0 = 1 << 27;  // set output to '0'
+
+//   register_value &= ~(0x7 << (7*3)); // clear the GPIO27 mode bits -> mode 0 (input)
+//  *gpfsel2 = register_value;     // set the new GPIFSEL2 register setting
+
+//   munmap(gpio_virt_addr_ptr, 0x1000); // free allocated memory
+
+//example using function calls
+  setup_gpio_regs();
+
+  set_gpio_mode(27, 1); // set GPIO 27 to output
+  set_gpio_out(27, 1);  // set GPIO 27 to high
   sleep(1);
-  *gpclr0 = 1 << 27;  // set output to '0'
+  set_gpio_out(27, 0);  // set GPIO 27 to low
 
-  register_value &= ~(0x7 << (7*3)); // clear the GPIO27 mode bits -> mode 0 (input)
- *gpfsel2 = register_value;     // set the new GPIFSEL2 register setting
-
-  munmap(gpio_virt_addr_ptr, 0x1000); // free allocated memory
-
-// example using function calls
-  // setup_gpio_regs();
-
-  // set_gpio_mode(27, 1); // set GPIO 17 to output
-  // set_gpio_out(27, 1);  // set GPIO 17 to high
-  // sleep(1);
-  // set_gpio_out(27, 0);  // set GPIO 17 to high
-
-  // cleanup(0);
+  cleanup(0);
   return(0);
 }
