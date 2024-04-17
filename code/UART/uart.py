447b1d2cc4cd9c83c3214019e20ead1620c8105c
Hans Krüger <krueger@physik.uni-bonn.de>
2024-04-17 14:10:58 +0200

    added serial0 to uart.py

--- code/UART/uart.py
+++ code/UART/uart.py
@@ -3,11 +3,12 @@ import RPi.GPIO as GPIO
 import time
 import serial.tools.list_ports
 
-# ports = serial.tools.list_ports.comports()
-# for port, desc, hwid in sorted(ports): # search device
-#   print("{}: {} [{}]".format(port, desc, hwid))
+ports = serial.tools.list_ports.comports(include_links=True)
+for port, desc, hwid in sorted(ports): # search device
+  print("{}: {} [{}]".format(port, desc, hwid))
 
-ser = serial.Serial('/dev/ttyS0', baudrate=9600)
+#ser = serial.Serial('/dev/ttyS0', baudrate=9600)
+ser = serial.Serial('/dev/serial0', baudrate=9600)
 if ser.is_open == False:
   print("Could not open serial port.")
   exit()
