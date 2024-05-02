743fb08032fe4762f0ab58f3104affc79011d5b2
Hans Krüger <krueger@physik.uni-bonn.de>
2024-05-02 20:48:44 +0200

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.intersphinx', 'sphinx_copybutton']
+extensions = ['sphinx.ext.intersphinx' 'sphinx_copybutton']
 
 source_suffix = {
     '.rst': 'restructuredtext',
