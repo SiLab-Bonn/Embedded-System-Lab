af9c945ef5ef264f1befe0fdfe0f57d508016710
Hans Krüger <krueger@physik.uni-bonn.de>
2024-05-02 18:03:54 +0200

    added cpld remote design implementation doc

--- code/AFE/CPLD/ISE/connect_remote_host.sh
+++ code/AFE/CPLD/ISE/connect_remote_host.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-remote_username="krueger"
+#set -x
 
-# Extract the local host name
+remote_username="pi"
+
+# Extract the local host name and set remote server name
 local_hostname=$(hostname)
 remote_hostname="asiclab001"
 
-# open ssh connection and create a directory on the remote host if not already present, mount remote folder and keep connection open afterwards
+# open ssh connection, create a directory on the remote host if not already present, mount remote folder and keep connection open afterwards
 ssh -t $remote_username@$remote_hostname.physik.uni-bonn.de \
-"[ ! -d $local_hostname-remote ] && mkdir  $local_hostname-remote " \
-"sshfs pi@$local_hostname.physik.uni-bonn.de:/home/pi/work $local_hostname-remote; bash -l"
+"[ ! -d $local_hostname-remote ] && mkdir $local_hostname-remote; " \
+"sshfs pi@$local_hostname.physik.uni-bonn.de:/home/pi/work $local_hostname-remote; "\
+"cd $local_hostname-remote; "\
+"bash -l"
 
-# intercative ssh connection 
+# intercative ssh connection now open
 
-# clean up sshfs mount
-fusermount -u $local_hostname-remote
-rmdir $local_hostname-remote
+
 
