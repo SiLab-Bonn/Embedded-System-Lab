ec9dc5a37e4ce77bd8219db8793ab7ced4cbf26c
Hans Krüger <krueger@physik.uni-bonn.de>
2024-05-02 23:08:56 +0200

    .

--- code/simple_fit.py
+++ code/simple_fit.py
@@ -9,10 +9,12 @@ y = 4 + 2 * x + np.random.randn(100)
 coefficients = np.polyfit(x, y, 1)
 fit = np.poly1d(coefficients)
 
+print(coefficients)
+
 # Plot the data points and the fitted line
 plt.scatter(x, y, label='Data')
 plt.plot(x, fit(x), color='red', label='Fit')
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.legend(['Data', f'Fit: {fit.coefficients[0]:.2f} x + {fit.coefficients[1]:.2f}'])
+plt.legend(['Data', f'Fit: y = {coefficients[0]:.2f} x + {coefficients[1]:.2f}'])
 plt.show()
