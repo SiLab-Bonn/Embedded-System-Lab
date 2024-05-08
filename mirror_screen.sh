d9ba70dcb223d9a87d40c5e3aa70c10e90d7798f
Hans Krüger <krueger@physik.uni-bonn.de>
2024-05-08 08:50:25 +0200

    added scripts

---
+++ mirror_screen.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo xrandr --output HDMI-1 --mode 1920x1080
+sudo xrandr --output HDMI-2 --mode 1920x1080
+sudo xrandr --output HDMI-1 --same-as HDMI-2
--- tmux_ini.sh
+++ tmux_ini.sh
@@ -6,9 +6,7 @@ ssh_list=( pi@pilab01.physik.uni-bonn.de \
            pi@pilab04.physik.uni-bonn.de \
            pi@pilab05.physik.uni-bonn.de \
            pi@pilab06.physik.uni-bonn.de \
-           pi@pilab07.physik.uni-bonn.de \
-           pi@pilab08.physik.uni-bonn.de)
-
+   )
 split_list=()
 for ssh_entry in "${ssh_list[@]:1}"; do
     split_list+=( split-pane -v -p 30 -h -p 50 ssh "$ssh_entry" ';' select-layout tiled ';' )
