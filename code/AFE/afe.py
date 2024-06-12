52a061a1d9a7a98d4722c15c51179137e78cfe2c
Hans Krüger <krueger@physik.uni-bonn.de>
2024-06-12 17:38:36 +0200

    fixed typo, afe.py cleanup

--- code/AFE/afe.py
+++ code/AFE/afe.py
@@ -45,7 +45,7 @@ def update_spi_regs(threshold, injected_signal, time_constant, out_mux):
   out_mux_val = out_mux_dict[out_mux]
 
   #set DAC channel A (threshold voltage)
-  spi_data = ((((0xfff & threshold) | dac_cmd_a) << 8) + \
+  spi_data = ((((0xfff & int(threshold)) | dac_cmd_a) << 8) + \
                 ( 0x07 & time_constant) | ((0x03 & out_mux_val) << 3))
   #print(bin(spi_data)[2:].zfill(24))
   spi.xfer(bytearray(spi_data.to_bytes(3, byteorder='big')))
@@ -65,111 +65,63 @@ out_mux = 0
 
 GPIO.output(INJECT, GPIO.LOW)
 
+def inject(threshold, charge, time_constant, n_injections, monitor = 'sha'):
+  update_spi_regs(threshold, charge, time_constant, monitor)
+  hit_count = 0
+  for i in range(n_injections): 
+    GPIO.output(INJECT, GPIO.HIGH) # inject charge
+    time.sleep(0.001) 
+    if (GPIO.input(COMP)):         # read latched comparator output
+      hit_count = hit_count + 1    
+    GPIO.output(INJECT, GPIO.LOW)  # reset charge injection and hit latch 
+    time.sleep(0.001)
+  return (hit_count/n_injections)  # return measured hit probability
 
-def threshold_scan(threshold, charge_range, time_constant_range, n_injections = 100, plot = 'sha'):
-  fig, ax = plt.subplots()
+def threshold_scan(threshold, charge_range, time_constant, n_injections = 100, monitor = 'sha', show_plot = False):
   hit_data = np.empty(0, int)
-  for time_constant in time_constant_range:
-    for charge in tqdm(charge_range):
-      update_spi_regs(threshold, charge, time_constant, plot)
-      hit_count = 0
-      for i in range (n_injections): 
-        GPIO.output(INJECT, GPIO.HIGH)
-        time.sleep(0.0001) 
-        if (GPIO.input(COMP)):
-          hit_count = hit_count + 1
-        GPIO.output(INJECT, GPIO.LOW)
-        time.sleep(0.0002)
-      hit_data=np.append(hit_data, hit_count/n_injections)
-    popt, pcov = curve_fit(err_func, charge_range, hit_data, bounds=([10,30], [100, 300]))
-    label_text = 'tau=%s, sigma=%.1f, threshold=%.1f' % (time_constants_list[time_constant], popt[0], popt[1])
+  for charge in tqdm(charge_range):        # scan range of injected charges
+    hit_probability = inject(threshold, charge, time_constant, n_injections, monitor)
+    hit_data=np.append(hit_data, hit_probability)
+  popt, pcov = curve_fit(err_func, charge_range, hit_data, bounds=([10,30], [100, 300])) # fir error function to data (still DAC units!)
+  print(popt)
+  if (show_plot == True):
+    fig, ax = plt.subplots()
+    label_text = 'tau=%s, sigma=%.1f, thr=%.1f [INJ_DAC], thr=%.1f [VTHR_DAC]' % (time_constants_list[time_constant], popt[0], popt[1], threshold)
     ax.plot(charge_range, hit_data, label=label_text)
-    print(popt)
     ax.plot(charge_range, err_func(charge_range, *popt))
-    hit_data = []
- 
-  ax.set(xlabel='charge (DAC)', ylabel='hit count', title='Threshold scan')
+    ax.set(xlabel='Injected charge (DAC)', ylabel='Hit probability', title='Threshold scan')
+    ax.legend()
+    ax.grid()
+    plt.show()
+  return popt, hit_data
+
+def parametric_threshold_scan(threshold_range, charge_range, time_constant_range, n_injections = 100, monitor = 'sha'):
+  hit_data = np.empty(0, int)
+  fig, ax = plt.subplots()
+  for time_constant in time_constant_range:    # scan range of shaper time constants or
+    for threshold in threshold_range:          # scan range of threshold voltages
+      popt, hit_data = threshold_scan(threshold, charge_range, time_constant, n_injections, monitor)
+      label_text = 'tau=%s, sigma=%.1f, thr=%.1f [INJ_DAC], thr=%.1f [VTHR_DAC]' % (time_constants_list[time_constant], popt[0], popt[1], threshold)
+      ax.plot(charge_range, hit_data, label=label_text)
+      ax.plot(charge_range, err_func(charge_range, *popt))
+  ax.set(xlabel='Injected charge (DAC)', ylabel='Hit probability', title='Threshold scan')
   ax.legend()
   ax.grid()
-  #fig.savefig("test.png")
   plt.show()
-
-
-def threshold_scan_3(threshold_range, charge_range, time_constant, n_injections = 100, plot = 'sha'):
-  fig, ax = plt.subplots()
-  hit_data = []
-  charge_data = []
-  for threshold in threshold_range:
-    for charge in tqdm(charge_range):
-      charge_data.append(charge)
-      update_spi_regs(threshold, charge, time_constant, plot)
-      hit_count = 0
-      for i in range (n_injections): 
-        GPIO.output(INJECT, GPIO.HIGH)
-        time.sleep(0.0001) 
-        if (GPIO.input(COMP)):
-          hit_count = hit_count + 1
-        GPIO.output(INJECT, GPIO.LOW)
-        time.sleep(0.0001)
-      hit_data.append(hit_count)
-    ax.plot(charge_data, hit_data, label=threshold)
-    charge_data = []
-    hit_data = []
- 
-  ax.set(xlabel='charge (DAC)', ylabel='hit count', title='Threshold scan')
-  ax.legend(title="threshold")
-  ax.grid()
-  #fig.savefig("test.png")
-  plt.show()
-
-def threshold_scan_2(threshold_range, charge, time_constant_range, n_injections = 100, plot = 'sha'):
-  fig, ax = plt.subplots()
-  hit_data = []
-  threshold_data = []
-  for time_constant in time_constant_range:
-    for threshold in tqdm(threshold_range):
-      threshold_data.append(threshold)
-      update_spi_regs(threshold, charge, time_constant, plot)
-      hit_count = 0
-      for i in range (n_injections): 
-        GPIO.output(INJECT, GPIO.HIGH)
-        time.sleep(0.0001) 
-        if (GPIO.input(COMP)):
-          hit_count = hit_count + 1
-        GPIO.output(INJECT, GPIO.LOW)
-        time.sleep(0.0001)
-      hit_data.append(hit_count)
-    ax.plot(threshold_data, hit_data, label=time_constants_list[time_constant])
-    threshold_data = []
-    hit_data = []
- 
-  ax.set(xlabel='threshold (DAC)', ylabel='hit count', title='Threshold scan')
-  ax.legend(title="shaper time constant")
-  ax.grid()
-  #fig.savefig("test.png")
-  plt.show() 
-
-
-def inject(threshold, charge, time_constant, n_injections):
-  update_spi_regs(threshold, charge, time_constant, 'sha')
-  for i in range(n_injections): 
-    GPIO.output(INJECT, GPIO.HIGH)
-    time.sleep(0.001) 
-    GPIO.output(INJECT, GPIO.LOW)
-    time.sleep(0.001)
   
 charge_range = np.arange(50, 301, 10, dtype=int)
-charge = 150
-threshold_range = np.arange(2300, 2701, 100, dtype=int)
-threshold = 2600
-time_constant_range = range(2,8)
+charge = 200
+threshold_range = np.arange(2400, 2701, 100, dtype=int)
+threshold = 2600 
+time_constant_range = range(6,7)
+time_constant = 6
 
-#threshold_scan_3(threshold_range, charge_range, 6, plot='sha')
-#threshold_scan_2(threshold_range, charge, time_constant_range, plot='sha')
-threshold_scan(threshold, charge_range, time_constant_range, n_injections=100, plot='sha')
+#inject(threshold, charge, time_constant, n_injections = 1000, monitor = 'sha')
+#threshold_scan(threshold, charge_range, time_constant, monitor='sha', show_plot = True)
+parametric_threshold_scan(threshold_range, charge_range, time_constant_range, monitor='sha')
 
 # while True:
-#   inject(2300, 2000, 6, 1)
+#   inject(2300, 300, 6, 1)
 
 #update_spi_regs(2000, 1000, 5, 'sha')
 #input()
