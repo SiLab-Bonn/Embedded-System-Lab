ccbcf07751d302b7b0a90c2fc41e983f240e44cb
Marco Vogt <vogt@physik.uni-bonn.de>
2026-07-13 18:18:12 +0200

    Update connect_remote_host.sh

--- code/AFE/CPLD/ISE/connect_remote_host.sh
+++ code/AFE/CPLD/ISE/connect_remote_host.sh
@@ -11,7 +11,7 @@ remote_hostname="asiclab005"
 # open ssh connection, create a directory on the remote host if not already present, mount remote folder and keep connection open afterwards
 ssh -t $remote_username@$remote_hostname.physik.uni-bonn.de \
 "[ ! -d $local_hostname-remote ] && mkdir $local_hostname-remote; " \
-"sshfs pi@$local_hostname.physik.uni-bonn.de:/home/pi/work $local_hostname-remote; "\
+"sshfs pi@$local_hostname.physik.uni-bonn.de:$(pwd) $local_hostname-remote; "\
 "cd $local_hostname-remote; "\
 "bash -l"
 
